0a4eb074d81d4e2a409792d8a3daafbbb4483e65
Ben Petro <benpetro@gmail.com>
2020-09-29 16:00:00 +1000

    boolean config fix

--- app/models/universal_ar/config_boolean.rb
+++ app/models/universal_ar/config_boolean.rb
@@ -2,12 +2,11 @@ class UniversalAr::ConfigBoolean < ApplicationRecord
   self.table_name = 'config_booleans'
   include UniversalAr::Concerns::Base
   include UniversalAr::Concerns::Polymorphic
-  
+
   base UniversalAr::ConfigBoolean, 'config_booleans'
-  
+
   belongs_to :configuration, optional: true
-  
-  validates :subject, :key, :value, presence: true
-  
+
+  validates :subject, :key, presence: true
+
 end
-  
