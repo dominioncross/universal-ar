06e57e9109a1b84e61f27242105d6e88ae35ee94
Ben Petro <benpetro@gmail.com>
2021-09-14 16:32:58 +1000

    Update users_controller.rb

--- app/controllers/universal_ar/users_controller.rb
+++ app/controllers/universal_ar/users_controller.rb
@@ -2,18 +2,19 @@ require_dependency "universal_ar/application_controller"
 
 module UniversalAr
   class UsersController < ApplicationController
-    
+
     def autocomplete
-      @users = User.all
-      if universal_scope
-        @users = @users.scoped_to(universal_scope)
-      end
+      @users =  if universal_scope
+                  universal_scope.users
+                else
+                  User.all
+                end
       if !params[:term].blank?
         @users = @users.where("email like ?", "%#{params[:term]}%")
       end
       json = @users.map{|c| {label: "#{c.name} - #{c.email}", value: c.id.to_s}}
       render json: json.to_json
     end
-    
+
   end
 end
