608ee427c6b679fca4929cf83f15c36eb5a15a6c
Ben Petro <benpetro@gmail.com>
2023-08-09 13:37:28 +1000

    Remove carrierwave

--- app/models/universal_ar/concerns/has_attachments.rb
+++
@@ -1,6 +0,0 @@
-module UniversalAr::Concerns::HasAttachments
-  extend ActiveSupport::Concern
-  included do
-    has_many :attachments, as: :subject, class_name: 'UniversalAr::Attachment'
-  end
-end
